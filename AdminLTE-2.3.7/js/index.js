036033bbbb9be6917be43d36446dcda6a390c68d
Longder <longder1989@qq.com>
2016-11-21 20:06:42 +0800

    Initial commit

---
+++ AdminLTE-2.3.7/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Longder on 2016/11/21.
+ */
+$(function(){
+    $("#menu_arrow").click(function(){
+        if($("#menu_arrow").hasClass("menu_arrow_left")){
+            $("#menu_arrow").animate({left:"40px"},290);
+            $("#menu_arrow").removeClass("menu_arrow_left");
+            $("#menu_arrow").addClass("menu_arrow_right");
+        }else if($("#menu_arrow").hasClass("menu_arrow_right")){
+            $("#menu_arrow").animate({left:"223px"},290);
+            $("#menu_arrow").removeClass("menu_arrow_right");
+            $("#menu_arrow").addClass("menu_arrow_left");
+        }
+    });
+});
